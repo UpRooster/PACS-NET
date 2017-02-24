abb140ebf04155b49fc91d118013b5e43a444de1
UpRooster <lberridge95@gmail.com>
2017-02-24 12:30:35 +0000

    Added console output

--- Network.cc
+++ Network.cc
@@ -22,6 +22,8 @@ int main (int argc, char *argv[])	// Declare Main Method
 	/* This area sets up basic debug logging and also the time resolution inside the simulation
          */
 
+	NS_LOG_INFO("Pre-Requisite Setup");
+	std::cout << "Pre-Requisite Setup.." << std::endl;
 	Time::SetResolution(Time::NS);	// Set time resolution to nanoseconds
 
 	LogComponentEnable("UdpEchoClientApplication", LOG_LEVEL_INFO);
@@ -36,6 +38,9 @@ int main (int argc, char *argv[])	// Declare Main Method
 	 * for those clusters
        	 */
 
+	NS_LOG_INFO("Network Pre-Setup");
+	std::cout << "Network Pre-Setup.." << std::endl;
+
 	NodeContainer nodes;		// Object for storing a node-set
 	nodes.Create(2);		// Create two nodes in the new group
 
@@ -45,6 +50,9 @@ int main (int argc, char *argv[])	// Declare Main Method
 	/* This area defines the topology type to be applied to the node clusters, as well as
 	 * defining attributes, such as DataRate and Delay
          */
+
+	NS_LOG_INFO("Creating Topology");
+	std::cout << "Creating Topology.." << std::endl;
 
 	// The P-T-P Helper assists with quick
 	// implementation of a topology to given nodes
@@ -63,6 +71,9 @@ int main (int argc, char *argv[])	// Declare Main Method
 	 * set up previously
 	 */
 
+	NS_LOG_INFO("Node Setup");
+	std::cout << "Node Setup.." << std::endl;
+
 	NetDeviceContainer devices;	// Object for storing simulated "Devices"
 	devices = pointToPoint.Install(nodes);	// Install "nodes" using pointToPoint inside "devices"
 
@@ -73,14 +84,23 @@ int main (int argc, char *argv[])	// Declare Main Method
 	 * create echo services for packets,
 	 */
 
+	NS_LOG_INFO("Stack Creation");
+	std::cout << "Stack Creation.." << std::endl;
+
 	InternetStackHelper stack;	// Instantiate the IS-Helper
 	stack.Install(nodes);		// Instantiate node attributes
+
+	NS_LOG_INFO("Address Assignment");
+	std::cout << "Address Assignment.." << std::endl;
 
 	Ipv4AddressHelper address;	// Instantiate the Ipv4-Helper
 	address.SetBase("10.1.1.0","255.255.255.0");	// Declare the base IP/mask
 
 	// Perform address assignment to all nodes in cluster "devices"
 	Ipv4InterfaceContainer interfaces = address.Assign(devices);
+
+	NS_LOG_INFO("Creating Server Echo Service");
+	std::cout << "Creating Server Echo Service.." << std::endl;
 
 	UdpEchoServerHelper echoServer(9); // Set a UDP port
 
@@ -90,6 +110,9 @@ int main (int argc, char *argv[])	// Declare Main Method
 	serverApps.Stop(Seconds(10.0));	// Disposed at X seconds of runtime
 
 	// Due to declaring a stop-time, the simulation will run for X seconds minimum
+
+	NS_LOG_INFO("Creating Client Echo Service");
+	std::cout << "Creating Client Echo Service" << std::endl;
 
 	// Get the physical address for nodes on port 9
 	UdpEchoClientHelper echoClient (interfaces.GetAddress(1),9);
@@ -111,8 +134,13 @@ int main (int argc, char *argv[])	// Declare Main Method
 	 * Simulator::Stop(Seconds(11.0));
 	 */
 
+	NS_LOG_INFO("Running Simulation");
+	std::cout << "\n-*-Running Simulation-*-\n" << std::endl;
+
 	// Standard Simulator Function (With no recurring events)
 	Simulator::Run();
+
+	std::cout << "\n-*-Closing Simulation-*-\n" << std::endl;
 
 	Simulator::Destroy();
 	return 0;
