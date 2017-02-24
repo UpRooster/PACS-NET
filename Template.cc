25c974b50c0c4c46336a958b420fff8818395d3e
UpRooster <lberridge95@gmail.com>
2017-02-24 12:40:33 +0000

    Updated Template

--- Template.cc
+++ Template.cc
@@ -1,4 +1,4 @@
-/* || Network Template     || C++
+/* || Initial Test Network || C++
  * || BER14475946	   ||
  * || Liam T. Berridge     ||
  */
@@ -11,12 +11,14 @@
 
 using namespace ns3; 			// Declare workspace
 
-NS_LOG_COMPONENT_DEFINE ("Init-Test"); 	// Log Components
+NS_LOG_COMPONENT_DEFINE ("Init-Test"); 	// Create Log "Init-Test"
 
 int main (int argc, char *argv[])	// Declare Main Method
 {
-	// ||||||||||||||||||||||||
-	// || PREREQUISITE SETUP ||
+	// || PREREQUISITE SETUP ||-------------------------------------------
+
+	NS_LOG_INFO("Pre-Requisite Setup");
+	std::cout << "Pre-Requisite Setup.." << std::endl;
 
 	Time::SetResolution(Time::NS);	// Set time resolution to nanoseconds
 
@@ -25,43 +27,52 @@ int main (int argc, char *argv[])	// Declare Main Method
 	LogComponentEnable("UdpEchoServerApplication", LOG_LEVEL_INFO);
 	// Enable Packet Logging -- Recieving
 
-	// ||||||||||||||||||||||
-	// || NETWORK PRESETUP ||
 
-	int clsSz = 2;	// Set cluster size (Global Var)
+	// || NETWORK PRESETUP |||-------------------------------------------
 
-	NodeContainer nodeCluster;	// Set cluster name
-	nodes.Create(clsSz);		// Set cluster size
+	NS_LOG_INFO("Network Pre-Setup");
+	std::cout << "Network Pre-Setup.." << std::endl;
 
-	// ||||||||||||||||||||
-	// || TOPOLOGY SETUP ||
+	int ndSz = 2;	// Define cluster size
 
-	
+	NodeContainer ndeCl;	// Define cluster object
+	ndeCl.Create(2);		// Apply cluster size
 
-	// Set bandwidth
-	topology.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
-	// Set ping (Network delay) - This delay stacks between nodes created with the helper
-	topology.SetChannelAttribute("Delay", StringValue("2ms"));
 
-	// |||||||||||||||||||
-	// || NETWORK SETUP ||
+	// || TOPOLOGY SETUP |||-------------------------------------------
 
-	/* This area begins to piece the network together using the clusters and topology
-	 * set up previously
-	 */
+	NS_LOG_INFO("Creating Topology");
+	std::cout << "Creating Topology.." << std::endl;
+
+	// Define topology object
+	// PointToPointHelper topologyDef;
+
+	// Define topology attributes
+		// Set bandwidth
+	topologyDef.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
+		// Set ping (Network delay) - This delay stacks between nodes created with the helper
+	topologyDef.SetChannelAttribute("Delay", StringValue("2ms"));
+
+
+	// || NETWORK SETUP |||-------------------------------------------
+
+	NS_LOG_INFO("Node Setup");
+	std::cout << "Node Setup.." << std::endl;
 
 	NetDeviceContainer devices;	// Object for storing simulated "Devices"
-	devices = pointToPoint.Install(nodes);	// Install "nodes" using pointToPoint inside "devices"
+	devices = topologyDef.Install(ndeCl);	// Install "nodes" using pointToPoint inside "devices"
 
-	// ||||||||||||||||||||||
-	// || SIMULATION SETUP ||
 
-	/* This area is where we setup devices, allow assignable attributes to devices,
-	 * create echo services for packets,
-	 */
+	// || SIMULATION SETUP |||-------------------------------------------
+
+	NS_LOG_INFO("Stack Creation");
+	std::cout << "Stack Creation.." << std::endl;
 
 	InternetStackHelper stack;	// Instantiate the IS-Helper
-	stack.Install(nodes);		// Instantiate node attributes
+	stack.Install(ndeCl);		// Instantiate node attributes
+
+	NS_LOG_INFO("Address Assignment");
+	std::cout << "Address Assignment.." << std::endl;
 
 	Ipv4AddressHelper address;	// Instantiate the Ipv4-Helper
 	address.SetBase("10.1.1.0","255.255.255.0");	// Declare the base IP/mask
@@ -69,14 +80,20 @@ int main (int argc, char *argv[])	// Declare Main Method
 	// Perform address assignment to all nodes in cluster "devices"
 	Ipv4InterfaceContainer interfaces = address.Assign(devices);
 
+	NS_LOG_INFO("Creating Server Echo Service");
+	std::cout << "Creating Server Echo Service.." << std::endl;
+
 	UdpEchoServerHelper echoServer(9); // Set a UDP port
 
 	// Installs "Echo" service on node/s to relay back confirmation of packets
-	ApplicationContainer serverApps = echoServer.Install(nodes.Get(1));
+	ApplicationContainer serverApps = echoServer.Install(ndeCl.Get(1));
 	serverApps.Start(Seconds(1.0)); // Called at X seconds of runtime
 	serverApps.Stop(Seconds(10.0));	// Disposed at X seconds of runtime
 
 	// Due to declaring a stop-time, the simulation will run for X seconds minimum
+
+	NS_LOG_INFO("Creating Client Echo Service");
+	std::cout << "Creating Client Echo Service" << std::endl;
 
 	// Get the physical address for nodes on port 9
 	UdpEchoClientHelper echoClient (interfaces.GetAddress(1),9);
@@ -87,19 +104,24 @@ int main (int argc, char *argv[])	// Declare Main Method
 	echoClient.SetAttribute("PacketSize",UintegerValue(1024));
 
 	//
-	ApplicationContainer clientApps = echoClient.Install(nodes.Get(0));
+	ApplicationContainer clientApps = echoClient.Install(ndeCl.Get(0));
 	clientApps.Start(Seconds(2.0));
 	clientApps.Stop(Seconds(10.0));
 
-	// |||||||||||||
-	// || RUN SIM ||
+
+	// || RUN SIM |||-------------------------------------------
 
 	/* For simulations with recursive events, the following can be implemented
 	 * Simulator::Stop(Seconds(11.0));
 	 */
 
+	NS_LOG_INFO("Running Simulation");
+	std::cout << "\n-*-Running Simulation-*-\n" << std::endl;
+
 	// Standard Simulator Function (With no recurring events)
 	Simulator::Run();
+
+	std::cout << "\n-*-Closing Simulation-*-\n" << std::endl;
 
 	Simulator::Destroy();
 	return 0;
