d81a70afe20d25d4dcbf45c18a14681ffb7fce4d
UpRooster <lberridge95@gmail.com>
2017-04-17 15:42:51 +0100

    Added functions to ring, restructured code

--- RING/Ring64.cc
+++ RING/Ring64.cc
@@ -10,73 +10,100 @@ using namespace ns3;
 
 NS_LOG_COMPONENT_DEFINE ("RING - 64 Nodes");
 
+void startUp ()
+{
+  Time::SetResolution(Time::NS);  // Set the simulation time resolution
+
+  LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);  // Enable client UDP logging - Unused without OnOffApplication
+  LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);  // Enable server UDP logging - Unused without OnOffApplication
+  Config::SetDefault ("ns3::OnOffApplication::PacketSize", UintegerValue (1500));   // Packet size (Bytes) - Unused without OnOffApplication
+  Config::SetDefault ("ns3::OnOffApplication::DataRate", StringValue ("1000kb/s")); // Data transmission rate for OnOffApplication - Unused without OnOffApplication
+}
+
+std::vector<NodeContainer> channelCreation (std::vector<NodeContainer> subnetList, NodeContainer Nodes)
+{
+  for(uint32_t i=0; i<subnetList.size(); ++i)
+  {
+    if (i == subnetList.size()-1)
+    {
+      NS_LOG_UNCOND ("Closing Ring!");
+      subnetList[i] = NodeContainer (Nodes.Get(i), Nodes.Get(0));
+    }
+    else
+    {
+      NS_LOG_UNCOND ("Creating Subnet " << i);
+      subnetList[i] = NodeContainer (Nodes.Get(i), Nodes.Get(i+1));
+    }
+  }
+  return subnetList;
+}
+
+std::vector<Ipv4InterfaceContainer> subnetCreation (std::vector<NetDeviceContainer> deviceList, std::vector<NodeContainer> subnetList, std::vector<Ipv4InterfaceContainer> subNetInterfaces, PointToPointHelper p2p)
+{
+  // CREATE IPV4 HELPER & IP STRING
+  Ipv4AddressHelper address;
+  std::ostringstream subnetAddr;
+  for(uint32_t i=0; i<deviceList.size(); ++i)
+  {
+    subnetAddr.str("");
+    deviceList[i] = p2p.Install (subnetList[i]);
+    subnetAddr <<"10.1."<<i+1<<".0";
+    NS_LOG_UNCOND ("Creating Address " << subnetAddr.str().c_str ());
+    address.SetBase(subnetAddr.str().c_str (),"255.255.255.0");
+    subNetInterfaces[i] = address.Assign(deviceList[i]);
+  }
+  return subNetInterfaces;
+}
+
 int main (int argc, char *argv[])
 {
   // Clinic
   NS_LOG_UNCOND ("Starting..");
-  Time::SetResolution(Time::NS);
-  LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);
-  LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
-  Config::SetDefault ("ns3::OnOffApplication::PacketSize", UintegerValue (1500)); // Limit of Ethernet
-  Config::SetDefault ("ns3::OnOffApplication::DataRate", StringValue ("1000kb/s")); // 1Mb dataRate
-  std::string animFile = "RING64.xml" ;  // Name of file for animation output
 
-  // Set Node Size
+  startUp();  // Call startup function
+
+  // SET VARIABLES
+  std::string animFile = "RING64.xml" ;  // Name of file for animation output
   uint32_t nNodes = 64;
   uint16_t timer = 60;
 
-  CommandLine cmd;
-  cmd.AddValue ("nNodes", "Nodes to place", nNodes); // Allow command line node setting
-  cmd.AddValue ("animFile",  "Name for Animation File", animFile); // Allow command line animation file naming
+  // PARSE COMMANDLINE
+  CommandLine cmd;        // Create CommandLine object
+  cmd.AddValue ("nNodes", "Nodes to place", nNodes);                // Add value for desired nodes from argc
+  cmd.AddValue ("animFile",  "Name for Animation File", animFile);  // Add value for output file name from argv
+  cmd.Parse (argc,argv);  // Parse argc, argv from commandline args (If present)
 
-  cmd.Parse (argc,argv);
+  // CREATE NODES
   NS_LOG_UNCOND ("Creating Nodes: " << nNodes);
-  // Init Nodes
   NodeContainer Nodes;
   Nodes.Create(nNodes);
 
-  // Init Stacks
+  // INSTALL INTERNET STACKS
   InternetStackHelper Stack;
   Stack.Install(Nodes);
 
-  // Init p2p (p2p)
+  // CREATE P2P HELPER
   PointToPointHelper p2p;
 
-  // Init Ipv4 + Address String
-  Ipv4AddressHelper address;
-  std::ostringstream subnetAddr;
+  // CREATE CHANNEL CONTAINER
   NS_LOG_UNCOND ("Creating Subnet List");
-  /*----------------SUBNET CREATION----------------*/
   std::vector<NodeContainer> subnetList (nNodes);
+
+  // CREATE CHANNELS
   NS_LOG_UNCOND ("Creating Subnets");
-  for(uint32_t i=0; i<subnetList.size(); ++i)
-    {
-      if (i == subnetList.size()-1)
-      {
-        NS_LOG_UNCOND ("Closing Ring!");
-        subnetList[i] = NodeContainer (Nodes.Get(i), Nodes.Get(0));
-      }
-      else
-      {
-        NS_LOG_UNCOND ("Creating Subnet " << i);
-        subnetList[i] = NodeContainer (Nodes.Get(i), Nodes.Get(i+1));
-      }
-    }
+  subnetList = channelCreation(subnetList, Nodes);
   uint16_t NSize =  subnetList.size();
+
+  // CREATE NET DEVICES
   NS_LOG_UNCOND ("Creating Devices");
-  /*----------------DEVICE CREATION----------------*/
   std::vector<NetDeviceContainer> deviceList (nNodes);
   std::vector<Ipv4InterfaceContainer> subNetInterfaces (nNodes);
-  for(uint32_t i=0; i<deviceList.size(); ++i)
-    {
-      subnetAddr.str("");
-      deviceList[i] = p2p.Install (subnetList[i]);
-      subnetAddr <<"10.1."<<i+1<<".0";
-      NS_LOG_UNCOND ("Creating Address " << subnetAddr.str().c_str ());
-      address.SetBase(subnetAddr.str().c_str (),"255.255.255.0");
-      subNetInterfaces[i] = address.Assign(deviceList[i]);
-    }
+
+  // ASSIGN CHANNELS ADDRESSES
+  NS_LOG_UNCOND ("Assigning Addresses to Channels");
+  subNetInterfaces = subnetCreation(deviceList, subnetList, subNetInterfaces, p2p);
   uint16_t ISize =  NSize-1;
+
   NS_LOG_UNCOND ("DeviceListSize: "<< ISize);
   NS_LOG_UNCOND ("Creating Address/App");
   /*----------------ADDRESS/APP CREATION----------------*/
@@ -95,20 +122,7 @@ int main (int argc, char *argv[])
   clientApps.Start (Seconds (2.0)); // Set open time
   clientApps.Stop (Seconds (timer)); // Set close time
 
-    /*Ipv4Address FS_Address(subNetInterfaces[1].GetAddress(1)); // Get Address of subNet Interfaces 1
-    uint16_t FS_Port = 4500;
-
-    UdpEchoClientHelper WKS1Echo(FS_Address, FS_Port);
-    ApplicationContainer WKS1EchoApp = WKS1Echo.Install(Nodes.Get (0)); // Install App
-    WKS1EchoApp.Start(Seconds(1.0));
-    WKS1EchoApp.Stop(Seconds(1.0));
-
-    UdpEchoServerHelper FS(FS_Port);
-    ApplicationContainer FS_App = FS.Install(subnetList[1].Get(0));
-    FS_App.Start(Seconds(1.0));
-    FS_App.Stop(Seconds(10.0));*/
   NS_LOG_UNCOND ("Creating Animation");
-  /*-----------------ANIMATION CREATION----------------*/
   Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
   AnimationInterface anim (animFile);
   for(uint32_t i=0; i<subnetList.size(); ++i)
